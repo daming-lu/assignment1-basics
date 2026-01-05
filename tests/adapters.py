f859564d3abd8273ff46f951416e24f7fac2068b
daming.lu <daming.lu@bytedance.com>
2026-01-05 10:34:51 -0800

    run_silu
    Change-Id: If6a61de9c9267add4343855e11dcd743936d67cc

--- tests/adapters.py
+++ tests/adapters.py
@@ -647,6 +647,7 @@ def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
         Float[Tensor,"..."]: of with the same shape as `in_features` with the output of applying
         SiLU to each element.
     """
+    print('TODO')
     raise NotImplementedError
 
 
